3a83dec124ba276711504f2d2b5c167ab046fdec
huy <116376784+Huykhoai@users.noreply.github.com>
2023-10-07 10:18:34 +0700

    Update TopFragment.java

--- main/java/com/example/thuvienphuongnam/fragment/TopFragment.java
+++ main/java/com/example/thuvienphuongnam/fragment/TopFragment.java
@@ -7,12 +7,22 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
+import com.example.thuvienphuongnam.Adapter.TopAdapter;
 import com.example.thuvienphuongnam.R;
+import com.example.thuvienphuongnam.dao.ThongkeDao;
+import com.example.thuvienphuongnam.model.Top;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class TopFragment extends Fragment {
-
+    ListView listView;
+    List<Top> arrayList;
+    TopAdapter adapter;
+    ThongkeDao dao;
 
 
     public TopFragment() {
@@ -38,6 +48,12 @@ public class TopFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_top, container, false);
+        View view =inflater.inflate(R.layout.fragment_top, container, false);
+        listView = view.findViewById(R.id.top_listview);
+        dao = new ThongkeDao(getActivity());
+        arrayList = dao.getTop();
+        adapter = new TopAdapter(arrayList,getContext());
+        listView.setAdapter(adapter);
+        return view;
     }
 }
